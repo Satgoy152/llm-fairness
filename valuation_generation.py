63f9aa1a7074a3de9a585f90cf412bde67876fdb
Satgoy152 <satyamgoyal@gmail.com>
2024-11-04 17:09:47 -0500

    Abstracted generation function

--- valuation_generation.py
+++ valuation_generation.py
@@ -1,10 +1,12 @@
 import numpy as np
 
-def generate_uniform_valuations(num_agents, num_items, scale=1.0):
-    return (np.random.uniform(0, 1, (num_agents, num_items)) * scale).round(2)
-
-def generate_exponential_valuations(num_agents, num_items, scale=1.0):
-    return (np.random.exponential(scale, (num_agents, num_items))).round(2)
-
-def generate_log_normal_valuations(num_agents, num_items, mean=0, sigma=1.0, scale=1.0):
-    return (np.random.lognormal(mean, sigma, (num_agents, num_items)) * scale).round(2)
+def generate_valuations(num_agents, num_items, scale=1.0, type='uniform'):
+    if type == 'uniform':
+        return (np.random.uniform(0, 1, (num_agents, num_items)) * scale).round(2)
+    elif type == 'exponential':
+        return (np.random.exponential(scale, (num_agents, num_items))).round(2)
+    elif type == 'lognormal':
+        return (np.random.lognormal(0, 1, (num_agents, num_items)) * scale).round(2)
+    else:
+        raise ValueError('Invalid type')
+    
