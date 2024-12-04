25081cd243ad1580e9f94d2e7a7205504927065a
Satgoy152 <satyamgoyal@gmail.com>
2024-12-04 15:20:15 -0500

    discrete values

--- valuation_generation.py
+++ valuation_generation.py
@@ -1,12 +1,20 @@
 import numpy as np
 
-def generate_valuations(num_agents, num_items, scale=1.0, type='uniform'):
+def generate_valuations(num_agents: int, num_items: int, scale=1.0, type='uniform') -> np.ndarray:
+    """
+    Generate valuations for agents for items
+    :param num_agents: Number of agents
+    :param num_items: Number of items
+    :param scale: Scale of the valuation
+    :param type: Type of distribution to use
+    :return: Valuation matrix
+    """
     if type == 'uniform':
-        return (np.random.uniform(0, 1, (num_agents, num_items)) * scale).round(2)
+        return (np.random.uniform(0, 1, (num_agents, num_items)) * scale).astype(int)
     elif type == 'exponential':
-        return (np.random.exponential(scale, (num_agents, num_items))).round(2)
+        return (np.random.exponential(scale, (num_agents, num_items))).astype(int)
     elif type == 'lognormal':
-        return (np.random.lognormal(0, 1, (num_agents, num_items)) * scale).round(2)
+        return (np.random.lognormal(0, 1, (num_agents, num_items)) * scale).astype(int)
     else:
         raise ValueError('Invalid type')
     
