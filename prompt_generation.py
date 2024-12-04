f91221a2643b4ff0def1747130acb7b573fc653e
Satgoy152 <satyamgoyal@gmail.com>
2024-12-04 15:20:08 -0500

    updated prompt for zero shot

--- prompt_generation.py
+++ prompt_generation.py
@@ -1,20 +1,21 @@
-def generate_prompts(agents: int, items: int, table):
+def generate_prompts(agents: int, items: int, table, prompt_type: str = 'zero_shot'):
     
-    base_prompt = f"""
-The following table represents the valuations of {agents} agents numbered 0,1,2, and so on for {items} items.  numbered 0,1,2, and so on. For example, the value agent 1 has of item 2 is {table[2][1]}.
+    if prompt_type == 'zero_shot':
+        base_prompt = f"""
+    The following table represents the valuations of {agents} agents numbered 0,1,2, and so on for {items} items numbered 0,1,2, and so on. For example, the value agent 1 has of item 2 is {table[2][1]}.
 
-{table}
+    {table}
 
-Fairly allocate all the items to the agents so that each agent gets an integer number of items.
+    Fairly allocate all the items to the agents so that each agent gets an integer number of items.
 
-Present your allocations at the end in the following json format:
-{{
-    "0": [0,1,2],
-    "1": [3,4],
-    "2": [5,6,7]
-}}
+    Present your allocations at the end in the following json format:
+    {{
+        "0": [0,1,2],
+        "1": [3,4],
+        "2": [5,6,7]
+    }}
 
-Where the keys are the agent numbers and the values are lists of the items allocated to that agent.
-"""
+    Where the keys are the agent numbers and the values are lists of the items allocated to that agent. The last think you give should be the allocations.
+    """
 
     return base_prompt
