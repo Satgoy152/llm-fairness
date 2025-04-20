997678fa10ed247ae5b9031b42f5032064742eb8
Satgoy152 <satyamgoyal@gmail.com>
2025-04-20 10:38:57 -0400

    persona prompting

--- prompt_generation.py
+++ prompt_generation.py
@@ -201,6 +201,79 @@ def generate_prompts(agents: int, items: int, table, prompt_type: str = 'zero_sh
     The last thing you give should be the allocations.
     """
 
+    elif prompt_type == 'persona_based2':
+        base_prompt = f"""
+        You are a computational economist with deep expertise in fair division and envy-freeness.
+
+        The following table represents the valuations of {agents} agents numbered 0,1,2, and so on for {items} items numbered 0,1,2, and so on. For example, the value agent 1 has of item 2 is {table[2][1]}.
+
+{table}
+
+    Fairly allocate all the items to the agents so that each agent gets an integer number of items.  Only give allocations for agents and items in the table above.
+    
+    Present your allocations at the end in the following json format:
+    {{
+        "0": [0,1,2,...],
+        "1": [3,4,...],
+        "2": [5,6,...],
+        "3": [7,8,...]
+    }}
+
+    Where the keys are the agent numbers and the values are lists of the items allocated to that agent.
+    Even if an agent is assigned no items, include them in the json with an empty list.
+    Make sure only and atleast {items} items are allocated in total.
+    The last thing you give should be the allocations.
+    """
+        
+    elif prompt_type == 'persona_based3':
+        base_prompt = f"""
+        You are a computational economist with deep expertise in fair division and envy-freeness.
+
+        The following table represents the valuations of {agents} agents numbered 0,1,2, and so on for {items} items numbered 0,1,2, and so on. For example, the value agent 1 has of item 2 is {table[2][1]}.
+{table}
+
+    Fairly allocate all the items to the agents so that each agent gets an integer number of items.  Only give allocations for agents and items in the table above.
+    
+    Present your allocations at the end in the following json format:
+    {{
+        "0": [0,1,2,...],
+        "1": [3,4,...],
+        "2": [5,6,...],
+        "3": [7,8,...],
+        "4": [9,10,...]
+    }}
+
+    Where the keys are the agent numbers and the values are lists of the items allocated to that agent.
+    Even if an agent is assigned no items, include them in the json with an empty list.
+    Make sure only and atleast {items} items are allocated in total.
+    The last thing you give should be the allocations.
+    """
+    
+    elif prompt_type == 'persona_based4':
+        base_prompt = f"""
+        You are a computational economist with deep expertise in fair division and envy-freeness.
+
+        The following table represents the valuations of {agents} agents numbered 0,1,2, and so on for {items} items numbered 0,1,2, and so on. For example, the value agent 1 has of item 2 is {table[2][1]}.
+    
+{table}
+
+    Fairly allocate all the items to the agents so that each agent gets an integer number of items.  Only give allocations for agents and items in the table above.
+    Present your allocations at the end in the following json format:
+    {{
+        "0": [0,1,2,...],
+        "1": [3,4,...],
+        "2": [5,6,...],
+        "3": [7,8,...],
+        "4": [9,10,...],
+        "5": [11,12,...]
+    }}
+
+    Where the keys are the agent numbers and the values are lists of the items allocated to that agent.
+    Even if an agent is assigned no items, include them in the json with an empty list.
+    Make sure only and atleast {items} items are allocated in total.
+    The last thing you give should be the allocations.
+    """
+
     else:
         print("Prompt type not found")
         return None
